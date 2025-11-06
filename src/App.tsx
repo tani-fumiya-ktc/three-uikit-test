4aa166a611fff197fef520896d7c0e9dda12cd2d
tani-fumiya-ktc <fumiya.tani@kinto-technologies.com>
2025-11-07 05:10:06 +0900

    feat: add Accordion component demo to App and update pnpm workspace dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Fullscreen, Container } from "@react-three/uikit";
+import { Container, Text } from "@react-three/uikit";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionTriggerIcon,
+} from "@react-three/uikit-default";
+
+function AccordionDemo() {
+  return (
+    <Container flexDirection="column" width={300}>
+      <Accordion>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <Text>Is it accessible?</Text>
+            <AccordionTriggerIcon />
+          </AccordionTrigger>
+          <AccordionContent>
+            <Text>Yes. It adheres to the WAI-ARIA design pattern.</Text>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            <Text>Is it styled?</Text>
+            <AccordionTriggerIcon />
+          </AccordionTrigger>
+          <AccordionContent>
+            <Text>
+              Yes. It comes with default styles that matches the other components&apos; aesthetic.
+            </Text>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            <Text>Is it animated?</Text>
+            <AccordionTriggerIcon />
+          </AccordionTrigger>
+          <AccordionContent>
+            <Text>Yes. It&apos;s animated by default, but you can disable it if you prefer.</Text>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </Container>
+  );
+}
 
 function App() {
   return (
-    <Canvas>
-      <Fullscreen>
-        <Container flexGrow={1} opacity={0.5} hover={{ opacity: 1 }} backgroundColor="red" />
-      </Fullscreen>
+    <Canvas
+      style={{ position: "absolute", inset: 0, touchAction: "none" }}
+      gl={{ localClippingEnabled: true }}
+    >
+      <OrbitControls />
+      <Container backgroundColor="red" sizeX={8} sizeY={4} flexDirection="row">
+        <Container flexGrow={1} margin={32} backgroundColor="green" />
+        <Container flexGrow={1} margin={32} backgroundColor="blue" />
+      </Container>
+      <AccordionDemo />
     </Canvas>
   );
 }
